4f10175b4aacf4b4af288358051dd66cc678e00e
Syahrial Agni Prasetya <syahrial@lemniskett.dev>
2023-11-14 15:34:00 +0700

    Improve soft restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-echo -e "===> Starting Odoo Server\n"
-python /opt/odoo/server/odoo-bin --pidfile /opt/odoo/server.pid --config /opt/odoo/etc/odoo.conf $@
-echo -e "\n===> Stopping Odoo Server"
+attempt_restart="true"
+
+while $attempt_restart; do
+    echo -e "===> Starting Odoo Server\n"
+    python /opt/odoo/server/odoo-bin --pidfile /opt/odoo/server.pid --config /opt/odoo/etc/odoo.conf $@
+    echo -e "\n===> Stopping Odoo Server"
+    attempt_restart="false"
+    if [[ -f /opt/odoo/soft-restart ]]; then
+        rm /opt/odoo/soft-restart
+        attempt_restart="true"
+    fi
+done
